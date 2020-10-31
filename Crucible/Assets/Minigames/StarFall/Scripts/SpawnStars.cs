e0b0da37bfd8c42415e21f39b55d4076396bab6c
StrangeAndStranger <71987096+StrangeAndStranger@users.noreply.github.com>
2020-10-31 18:19:00 -0400

    Added star generator (Uncomplete)

---
+++ Crucible/Assets/Minigames/StarFall/Scripts/SpawnStars.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStars : MonoBehaviour
+{   
+    public GameObject Star;
+    public int spawnRate = 30;
+    public float disappearTimer = 5f;
+    int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(count / spawnRate == 0)
+            {
+                GameObject newStars = GameObject.Instantiate(Star);
+                newStars.transform.position = new Vector3(Random.Range(-10f, 10f), 6f, 0f);
+                Destroy(newStars, disappearTimer);
+            }
+        count = count + 1;
+        
+    }
+}
